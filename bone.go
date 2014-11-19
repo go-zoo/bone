b794ff2d775c694604c6384e68d01d2ec0c6d7c0
squiidz <piinkyz05@gmail.com>
2014-11-18 21:17:10 -0500

    First Commit

---
+++ bone.go
@@ -0,0 +1,41 @@
+package bone
+
+import "net/http"
+
+type Mux struct {
+	Handlers map[string]http.Handler
+	NotFound http.Handler
+}
+
+func NewMux() *Mux {
+	return &Mux{make(map[string]http.Handler), nil}
+}
+
+func (m *Mux) SetNotFound(h http.Handler) {
+	m.NotFound = h
+}
+
+func (m *Mux) Handle(path string, h http.Handler) {
+	if path != "" {
+		m.Handlers[path] = h
+	} else {
+		panic("Non-Valid Path")
+	}
+}
+
+func (m *Mux) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	var valid = ""
+	for k, _ := range m.Handlers {
+		if req.URL.Path == k {
+			valid = k
+		}
+	}
+	if valid != "" {
+		m.Handlers[valid].ServeHTTP(rw, req)
+	} else {
+
+		m.NotFound.ServeHTTP(rw, req)
+
+	}
+
+}
