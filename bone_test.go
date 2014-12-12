04e2c0dd04f000d83cb0bd05313505fc0b59d1a6
squiidz <piinkyz05@gmail.com>
2014-12-11 21:48:30 -0500

    add TestRoutingMethod

--- bone_test.go
+++ bone_test.go
@@ -21,3 +21,19 @@ func TestRouting(t *testing.T) {
 		t.Error("handler should not be called")
 	}
 }
+
+func TestRoutingMethod(t *testing.T) {
+	mux := New()
+	call := false
+	mux.Get("/t", func(http.ResponseWriter, *http.Request) {
+		call = true
+	})
+
+	r, _ := http.NewRequest("POST", "/t", nil)
+	w := httptest.NewRecorder()
+	mux.ServeHTTP(w, r)
+
+	if call {
+		t.Error("response to a wrong method")
+	}
+}
