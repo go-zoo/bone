791cf49e364c380bd9fd8946438da4223217a5ed
sahil <sahil.agg15@gmail.com>
2015-01-05 08:02:05 +0530

    Logger

--- bone.go
+++ bone.go
@@ -43,7 +43,6 @@ func (m *Mux) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	// Check if the request path doesn't end with /
 	if !valid(reqPath) {
 		http.Redirect(rw, req, reqPath[:reqLen-1], http.StatusMovedPermanently)
-		logger(rw,req)
 		return
 	}
 	// Loop over all the registred route.
@@ -51,13 +50,11 @@ func (m *Mux) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		// If the route is equal to the request path.
 		if reqPath == r.Path {
 			r.Handler.ServeHTTP(rw, req)
-			logger(rw,req)
 			return
 		} else if r.Pattern.Exist {
 			if v, ok := r.Match(req.URL.Path); ok {
 				r.insert(req, v)
 				r.Handler.ServeHTTP(rw, req)
-				logger(rw,req)
 				return
 			}
 			// If no pattern are set in the route.
@@ -69,24 +66,25 @@ func (m *Mux) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	for _, s := range m.Static {
 		if reqLen >= s.Size && reqPath[:s.Size] == s.Path {
 			s.ServeHTTP(rw, req)
-			logger(rw,req)
 			return
 		}
 		continue
 	}
-	logger(rw,req)
 	m.BadRequest(rw, req)
 }
 
 // Logger is used to print the access logs with time taken to complete the request(ToDo: Status Code)
-func logger(rw http.ResponseWriter,req *http.Request) {
-	start := time.Now()
-	log.Printf(
-		"%s\t%s\t%s",
-		req.Method,
-		req.RequestURI,
-		time.Since(start),
-		)
+func Logger(inner http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func (rw http.ResponseWriter,req *http.Request) {
+		start := time.Now()
+		inner.ServeHTTP(rw,req)
+		log.Printf(
+			"%s\t%s\t%s",
+			req.Method,
+			req.RequestURI,
+			time.Since(start),
+			)
+	})
 }
 
 // HandleFunc is use to pass a func(http.ResponseWriter, *Http.Request) instead of http.Handler
@@ -96,7 +94,7 @@ func (m *Mux) HandleFunc(path string, handler http.HandlerFunc) {
 
 // Handle add a new route to the Mux without a HTTP method
 func (m *Mux) Handle(path string, handler http.Handler) {
-	r := NewRoute(path, handler)
+	r := NewRoute(path, Logger(handler))
 	if m.isStatic(path) {
 		m.Static = append(m.Static, r.Get())
 		return
@@ -110,54 +108,54 @@ func (m *Mux) Handle(path string, handler http.Handler) {
 
 // Get add a new route to the Mux with the Get method
 func (m *Mux) Get(path string, handler http.Handler) {
-	r := NewRoute(path, handler)
+	r := NewRoute(path, Logger(handler))
 	m.Routes["GET"] = append(m.Routes["GET"], r.Get())
 	sort.Sort(ByLength(m.Routes["GET"]))
 }
 
 // Post add a new route to the Mux with the Post method
 func (m *Mux) Post(path string, handler http.Handler) {
-	r := NewRoute(path, handler)
+	r := NewRoute(path, Logger(handler))
 	m.Routes["POST"] = append(m.Routes["POST"], r.Post())
 	sort.Sort(ByLength(m.Routes["POST"]))
 }
 
 // Put add a new route to the Mux with the Put method
 func (m *Mux) Put(path string, handler http.Handler) {
-	r := NewRoute(path, handler)
+	r := NewRoute(path, Logger(handler))
 	m.Routes["PUT"] = append(m.Routes["PUT"], r.Put())
 	sort.Sort(ByLength(m.Routes["PUT"]))
 }
 
 // Delete add a new route to the Mux with the Delete method
 func (m *Mux) Delete(path string, handler http.Handler) {
-	r := NewRoute(path, handler)
+	r := NewRoute(path, Logger(handler))
 	m.Routes["DELETE"] = append(m.Routes["DELETE"], r.Delete())
 	sort.Sort(ByLength(m.Routes["DELETE"]))
 }
 
 // Head add a new route to the Mux with the Head method
 func (m *Mux) Head(path string, handler http.Handler) {
-	r := NewRoute(path, handler)
+	r := NewRoute(path, Logger(handler))
 	m.Routes["HEAD"] = append(m.Routes["HEAD"], r.Head())
 	sort.Sort(ByLength(m.Routes["HEAD"]))
 }
 
 // Patch add a new route to the Mux with the Patch method
 func (m *Mux) Patch(path string, handler http.Handler) {
-	r := NewRoute(path, handler)
+	r := NewRoute(path, Logger(handler))
 	m.Routes["PATCH"] = append(m.Routes["PATCH"], r.Patch())
 	sort.Sort(ByLength(m.Routes["PATCH"]))
 }
 
 // Options add a new route to the Mux with the Options method
 func (m *Mux) Options(path string, handler http.Handler) {
-	r := NewRoute(path, handler)
+	r := NewRoute(path, Logger(handler))
 	m.Routes["OPTIONS"] = append(m.Routes["OPTIONS"], r.Options())
 	sort.Sort(ByLength(m.Routes["OPTIONS"]))
 }
 
 // Set the mux custom 404 handler
 func (m *Mux) NotFound(handler http.HandlerFunc) {
-	m.notFound = handler
+	m.notFound = Logger(handler)
 }
